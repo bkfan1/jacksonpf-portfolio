b2c72143e968c19cd8af5c1f3d297b4431771620
bkfan1 <jpf8296@gmail.com>
2023-07-28 13:40:26 -0400

    Fix responsive styles

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,5 @@
 import {
   Avatar,
-  Box,
   Button,
   ButtonGroup,
   ChakraProvider,
@@ -26,6 +25,7 @@ import {
   BsAwardFill,
   BsBriefcaseFill,
   BsEnvelope,
+  BsFillFileTextFill,
   BsGearFill,
   BsGithub,
   BsHouseFill,
@@ -38,6 +38,7 @@ import MediaButton from "./components/MediaButton";
 import { nanoid } from "nanoid";
 import TimelineStepper from "./components/TimelineStepper";
 import useIsMounted from "./hooks/useIsMounted";
+import HomeHero from "./components/HomeHero";
 
 const workExperience = [
   {
@@ -80,236 +81,214 @@ const education = [
 ];
 
 function App() {
-
-  const {isMounted} = useIsMounted();
+  const { isMounted } = useIsMounted();
 
   return (
     <>
       <ChakraProvider>
         {isMounted ? (
           <>
-          <Tabs align={"end"} variant={"enclosed"}>
-          <TabList px={2} pt={2}>
-            <Tab>
-              <Icon as={BsHouseFill} mr={2} />
-              <Text display={{ sm: "none", md: "flex" }}>Home</Text>
-            </Tab>
-            <Tab>
-              <Icon as={BsPersonVcardFill} mr={2} />
-              <Text display={{ sm: "none", md: "flex" }}>About</Text>
-            </Tab>
-            <Tab>
-              <Icon as={BsBriefcaseFill} mr={2} />
-              <Text display={{ sm: "none", md: "flex" }}>Portfolio</Text>
-            </Tab>
-            <Tab>
-              <Icon as={BsJournalBookmarkFill} mr={2} />
-              <Text display={{ sm: "none", md: "flex" }}>Contact</Text>
-            </Tab>
-          </TabList>
-          <TabPanels>
-            <TabPanel id="panelsito">
-              <Flex minHeight={"100vh"} justifyContent={"center"}>
-                <Stack
-                  direction={{ base: "row", sm: "column", md: "row" }}
-                  gap={4}
-                  mt={44}
+            <Tabs align={"end"} variant={"enclosed"}>
+              <TabList px={2} pt={2}>
+                <Tab>
+                  <Icon as={BsHouseFill} />
+                  <Text display={{ base: "none", md: "flex" }} ml={2}>
+                    Home
+                  </Text>
+                </Tab>
+                <Tab>
+                  <Icon as={BsPersonVcardFill} />
+                  <Text display={{ base: "none", md: "flex" }} ml={2}>
+                    About
+                  </Text>
+                </Tab>
+                <Tab>
+                  <Icon as={BsBriefcaseFill} />
+                  <Text display={{ base: "none", md: "flex" }} ml={2}>
+                    Portfolio
+                  </Text>
+                </Tab>
+                <Tab>
+                  <Icon as={BsJournalBookmarkFill} />
+                  <Text display={{ base: "none", md: "flex" }} ml={2}>
+                    Contact
+                  </Text>
+                </Tab>
+              </TabList>
+              <TabPanels>
+                <TabPanel
+                  backgroundImage={`url(/polygon.svg)`}
+                  backgroundRepeat={"no-repeat"}
+                  backgroundPosition={{ base: "top", md: "revert" }}
+                  backgroundSize={"cover"}
                 >
-                  <Avatar
-                    size={"2xl"}
-                    src="/jack.jpg"
-                    alignSelf={{ sm: "center", md: "initial" }}
-                  />
-                  <VStack textAlign={{ sm: "center", md: "left" }}>
-                    <Heading width={"100%"}>Jackson Paredes Ferranti</Heading>
-                    <Heading width={"100%"} size={"2xl"} textColor={"blue.500"}>
-                      Web Developer
-                    </Heading>
-                    <Box width={"100%"}>
-                      <Text width={"md"} lineHeight={2}>
-                        <Highlight
-                          query={[
-                            "focused on creating solutions",
-                            "in backend and frontend",
-                          ]}
-                          styles={{
-                            px: 2,
-                            py: 1,
-                            my: 1,
-                            rounded: "full",
-                            fontWeight: "bold",
-                            color: "white",
-                            bg: "blue.500",
-                          }}
-                        >
-                          A software developer focused on creating solutions
-                          using modern web technologies, both in backend and
-                          frontend.
-                        </Highlight>
-                      </Text>
-                    </Box>
-
-                    <ButtonGroup mt={2} width={"100%"}>
-                      <Button
-                        width={{ sm: "100%", md: "initial" }}
-                        colorScheme="blue"
-                        variant={"outline"}
+                  <Flex minHeight={"100vh"} justifyContent={"center"}>
+                    <HomeHero />
+                  </Flex>
+                </TabPanel>
+                <TabPanel>
+                  <Flex flexDirection={"column"} minHeight={"100vh"} px={16}>
+                    <VStack width={"100%"} mb={6}>
+                      <Heading
+                        width={"100%"}
+                        textAlign={{ base: "center", md: "left" }}
                       >
-                        Contact me
-                      </Button>
-                    </ButtonGroup>
-                  </VStack>
-                </Stack>
-              </Flex>
-            </TabPanel>
-            <TabPanel>
-              <Flex
-                flexDirection={"column"}
-                minHeight={"100vh"}
-                px={16}
-              >
-                <VStack width={"100%"} mb={6}>
-                  <Heading
-                    width={"100%"}
-                    textAlign={{ sm: "center", md: "left" }}
-                  >
-                    About me
-                  </Heading>
-                  <Stack width={"100%"} direction={{ sm: "column", md: "row" }}>
-                    <Avatar
-                      src="/jack2.jpg"
-                      size={"2xl"}
-                      alignSelf={{ sm: "center", md: "initial" }}
-                    />
-                    <VStack>
-                      <Text
-                        maxWidth={"2xl"}
-                        my={{ sm: 2 }}
-                        textAlign={"justify"}
+                        About me
+                      </Heading>
+                      <Stack
+                        width={"100%"}
+                        direction={{ base: "column", md: "row" }}
                       >
-                        <Highlight
-                          query={[
-                            "software developer",
-                            "always passionate, curious",
-                            "detail-oriented",
-                          ]}
-                          styles={{
-                            px: 2,
-                            py: 1,
-                            rounded: "full",
-                            fontWeight: "bold",
-                            color: "white",
-                            bg: "blue.500",
-                          }}
-                        >
-                          Jackson Paredes Ferranti is a software developer
-                          currently focused on web development, but he plans to
-                          advance his career by learning data science and
-                          becoming a Data Engineer. He is always passionate,
-                          curious, and interested in computer science topics
-                          such as data science, artificial intelligence, data
-                          structures, algorithms, and many more. As a worker, he
-                          is detail-oriented and likes to explore new ideas and
-                          approaches to get the job done.
-                        </Highlight>
-                      </Text>
-                      <ButtonGroup width={"100%"}>
-                        <Link href="/CV_JacksonParedesFerranti_2023.pdf">
-                          <Button
-                            colorScheme="green"
-                            width={{ sm: "100%", md: "initial" }}
+                        <Avatar
+                          src="/jack2.jpg"
+                          size={"2xl"}
+                          alignSelf={{ base: "center", md: "initial" }}
+                        />
+                        <VStack>
+                          <Text
+                            maxWidth={"2xl"}
+                            my={{ sm: 2 }}
+                            textAlign={"justify"}
                           >
-                            Download CV
-                          </Button>
-                        </Link>
-                      </ButtonGroup>
+                            <Highlight
+                              query={[
+                                "software developer",
+                                "always passionate, curious",
+                                "detail-oriented",
+                              ]}
+                              styles={{
+                                px: 2,
+                                py: 1,
+                                rounded: "full",
+                                fontWeight: "bold",
+                                color: "white",
+                                bg: "blue.500",
+                              }}
+                            >
+                              Jackson Paredes Ferranti is a software developer
+                              currently focused on web development, but he plans
+                              to advance his career by learning data science and
+                              becoming a Data Engineer. He is always passionate,
+                              curious, and interested in computer science topics
+                              such as data science, artificial intelligence,
+                              data structures, algorithms, and many more. As a
+                              worker, he is detail-oriented and likes to explore
+                              new ideas and approaches to get the job done.
+                            </Highlight>
+                          </Text>
+                          <ButtonGroup width={"100%"}>
+                            <Link
+                              href="/CV_JacksonParedesFerranti_2023.pdf"
+                              width={"100%"}
+                            >
+                              <Button
+                                colorScheme="green"
+                                width={{ base: "100%", md: "initial" }}
+                                leftIcon={<BsFillFileTextFill />}
+                              >
+                                Download CV
+                              </Button>
+                            </Link>
+                          </ButtonGroup>
+                        </VStack>
+                      </Stack>
                     </VStack>
-                  </Stack>
-                </VStack>
-                <Divider />
+                    <Divider />
 
-                <VStack width={"100%"} my={6}>
-                  <Heading
-                    width={"100%"}
-                    textAlign={{ sm: "center", md: "right" }}
+                    <VStack width={"100%"} my={6}>
+                      <Heading
+                        width={"100%"}
+                        textAlign={{ base: "center", md: "right" }}
+                      >
+                        Education
+                      </Heading>
+                      <TimelineStepper timelineData={education} />
+                    </VStack>
+
+                    <Divider />
+
+                    <VStack width={"100%"} my={6}>
+                      <Heading
+                        width={"100%"}
+                        textAlign={{ base: "center", md: "left" }}
+                        mb={2}
+                      >
+                        Work Experience
+                      </Heading>
+                      <TimelineStepper timelineData={workExperience} />
+                    </VStack>
+                  </Flex>
+                </TabPanel>
+                <TabPanel>
+                  <Flex
+                    flexDirection={"column"}
+                    minHeight={"100vh"}
+                    gap={4}
+                    px={16}
                   >
-                    Education
-                  </Heading>
-                  <TimelineStepper timelineData={education} />
-                </VStack>
+                    <VStack width={"100%"}>
+                      <Heading width={"100%"}>Portfolio</Heading>
+                      <Text width={"100%"}>
+                        Projects I would like to highlight
+                      </Text>
+                    </VStack>
+                    <Divider />
+                    <PortfolioGrid />
+                  </Flex>
+                </TabPanel>
+                <TabPanel>
+                  <Flex width={"100%"} minHeight={"100vh"}>
+                    <VStack margin={"auto"} spacing={8}>
+                      <Heading>You can reach me on:</Heading>
+                      <SimpleGrid columns={{ sm: 2, md: 3, lg: 4 }} spacing={6}>
+                        <MediaButton
+                          icon={BsEnvelope}
+                          username={"jpf8296@gmail.com"}
+                          link={"mailto:jpf8296@gmail.com"}
+                        />
+                        <MediaButton
+                          icon={BsPhone}
+                          username={"+584243529011"}
+                          link={"https://wa.link/4burb4"}
+                        />
+                        <MediaButton
+                          icon={BsLinkedin}
+                          username={"linkedin.com/in/jacksonpf"}
+                          link={"https://www.linkedin.com/in/jacksonpf1"}
+                        />
 
-                <Divider />
-
-                <VStack width={"100%"} my={6}>
-                  <Heading
-                    width={"100%"}
-                    textAlign={{ sm: "center", md: "left" }}
-                    mb={2}
-                  >
-                    Work Experience
-                  </Heading>
-                  <TimelineStepper timelineData={workExperience} />
-                </VStack>
-
-                <Divider />
-              </Flex>
-            </TabPanel>
-            <TabPanel>
-              <Flex
-                flexDirection={"column"}
-                minHeight={"100vh"}
-                gap={4}
-                px={16}
-              >
-                <VStack width={"100%"}>
-                  <Heading width={"100%"}>Portfolio</Heading>
-                  <Text width={"100%"}>Projects I would like to highlight</Text>
-                </VStack>
-                <PortfolioGrid />
-              </Flex>
-            </TabPanel>
-            <TabPanel>
-              <Flex width={"100%"} minHeight={"100vh"}>
-                <VStack margin={"auto"} spacing={8}>
-                  <Heading>You can reach me on</Heading>
-                  <SimpleGrid columns={{ sm: 2, md: 3, lg: 4 }} spacing={6}>
-                    <MediaButton
-                      icon={BsEnvelope}
-                      username={"jpf8296@gmail.com"}
-                      link={"mailto:jpf8296@gmail.com"}
-                    />
-                    <MediaButton
-                      icon={BsPhone}
-                      username={"+584243529011"}
-                      link={"https://wa.link/4burb4"}
-                    />
-                    <MediaButton
-                      icon={BsLinkedin}
-                      username={"linkedin.com/in/jacksonpf"}
-                      link={"https://www.linkedin.com/in/jacksonpf1"}
-                    />
-
-                    <MediaButton
-                      icon={BsGithub}
-                      username={"bkfan1"}
-                      link={"https://www.github.com/bkfan1"}
-                    />
-                  </SimpleGrid>
-                </VStack>
-              </Flex>
-            </TabPanel>
-          </TabPanels>
-        </Tabs>
-        <Divider/>
-        <Flex py={4} alignItems={"center"} justifyContent={"center"}>
-          <Text>Created by Jackson Paredes Ferranti</Text>
-        </Flex>
+                        <MediaButton
+                          icon={BsGithub}
+                          username={"bkfan1"}
+                          link={"https://www.github.com/bkfan1"}
+                        />
+                      </SimpleGrid>
+                    </VStack>
+                  </Flex>
+                </TabPanel>
+              </TabPanels>
+            </Tabs>
+            <Divider />
+            <Flex py={4} alignItems={"center"} justifyContent={"center"}>
+              <Text>
+                <Highlight
+                  query={"Jackson Paredes Ferranti"}
+                  styles={{ color: "blue.500", fontWeight: "bold" }}
+                >
+                  Created by Jackson Paredes Ferranti 👨‍💻
+                </Highlight>
+              </Text>
+            </Flex>
           </>
         ) : (
           <>
-          <Flex minHeight={"100vh"} justifyContent={"center"} alignItems={"center"}>
-          <Spinner size={"xl"} color="blue.500" />
-          </Flex>
+            <Flex
+              minHeight={"100vh"}
+              justifyContent={"center"}
+              alignItems={"center"}
+            >
+              <Spinner size={"xl"} color="blue.500" />
+            </Flex>
           </>
         )}
       </ChakraProvider>
