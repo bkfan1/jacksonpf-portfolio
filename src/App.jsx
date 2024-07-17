8c9189fa086645cbade3739572742cc284a95b65
bkfan1 <jpf8296@gmail.com>
2024-07-17 16:53:24 -0400

    Remove deleted Page component

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,6 @@ import { Route, Routes } from "react-router-dom";
 import IndexPage from "./components/pages/IndexPage";
 import PortfolioPage from "./components/pages/PortfolioPage";
 import AboutPage from "./components/pages/AboutPage";
-import ContactPage from "./components/pages/ContactPage";
 import { ChakraProvider } from "@chakra-ui/react";
 import RootLayout from "./components/layouts/RootLayout";
 import theme from "./theme";
@@ -16,7 +15,6 @@ function App() {
             <Route path="/" element={<IndexPage />} />
             <Route path="/portfolio" element={<PortfolioPage />} />
             <Route path="/about" element={<AboutPage />} />
-            <Route path="/contact" element={<ContactPage />} />
           </Route>
         </Routes>
       </ChakraProvider>
